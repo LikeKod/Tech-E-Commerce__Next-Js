d9f1e826e8e5890934de0cbd3f53626eb9a72b45
LikeKod <i.serebryanoy@yandex.ru>
2023-12-18 23:59:04 +0300

    add_components

---
+++ utils/data.js
@@ -0,0 +1,10 @@
+const data = {
+    cards_category:[
+        {
+            image: '/images/cards_category/watch.png',
+            name: 'Popular Products',
+            description: 'iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.',
+            txt_btn: 'shop now',
+        }
+    ]
+}
