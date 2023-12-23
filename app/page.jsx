2c83003679d0066d7abe7a8cbd535e54ffd77935
LikeKod <i.serebryanoy@yandex.ru>
2023-12-23 23:01:14 +0300

    does_not_work

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Footer from ''
+import Header from "./ui/header/header"
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <Footer></Footer>
+      <Header />
     </main>
   )
 }
