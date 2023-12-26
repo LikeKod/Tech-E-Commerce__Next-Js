65b962a38b02a74bd791d3872857274630f2d154
LikeKod <i.serebryanoy@yandex.ru>
2023-12-26 23:22:08 +0300

    try_button

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Header from "./ui/header/header"
 import Footer from "./ui/footer/Footer"
+import BasicButtons from './ui/components/Button.jsx'
 
 export default function Home() {
   return (
     <main >
-      <Header/>
+      <Header />
+      {/* <BasicButtons /> */}
       <Footer />
     </main>
   )
