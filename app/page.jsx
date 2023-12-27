24f61e96065f5763a40d8c9fda03f62cb9660e9b
Mairans <mairanschaika@gmail.com>
2023-12-28 00:54:41 +0300

    Добавил баннеры

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,15 @@ import styles from './page.module.css'
 import Header from "./ui/header/header"
 import Footer from "./ui/footer/Footer"
 import BasicButtons from './ui/components/Button.jsx'
+import Banner from "./ui/banner/Banner"
+import Banners from "./ui/smallerBanners/smallersBanners"
 
 export default function Home() {
   return (
     <main >
       <Header />
+      <Banner />
+      <Banners />
       {/* <BasicButtons /> */}
       <Footer />
     </main>
---
+++ app/ui/banner/Banner.jsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Iphone from '/public/images/banner/iphone.png';
+import './Banner.css'
+
+export default function Banner() {
+  return (
+    <div className={'banner'}>
+      <div className={'info'}>
+        <span>Pro.Beyond</span>
+        <>
+          <h1 className={'tittle'}>IPhone 14 Pro</h1>
+          <p className={"description"}>Created to change everything for the better. For everyone</p>
+        </>
+        <>
+          <a href="#" className={"btn"}>Shop Now</a>
+        </>
+      </div>
+      <div className={'iphone'}>
+        <Image
+          priority
+          src={Iphone}
+          alt={'iphone'} />
+      </div>
+    </div>
+  )
+}
