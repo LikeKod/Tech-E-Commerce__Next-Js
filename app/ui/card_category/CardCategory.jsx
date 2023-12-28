769d0e1fe16169c4ad9b4a54bf7a3ed827a97a4f
LikeKod <i.serebryanoy@yandex.ru>
2023-12-29 00:13:23 +0300

    adapt_card_category-continue

--- app/ui/card_category/CardCategory.jsx
+++ app/ui/card_category/CardCategory.jsx
@@ -12,11 +12,10 @@ import image1 from "/public/images/card_category/image 12.png";
 
 export default function CardCategory() {
     return (
-
-        <section className={"card__category"}>
-            {/* <div className={"card__category__container"}>
-                <div className={"card__category__item item-1"}>
-                    <div className={"card__category-img"}>
+        <section className="w-full flex mx-auto">
+            <div className="flex mx-auto hidden md:flex">
+                <div className="items-center text-center max-w-[360px] h-[640px]">
+                    <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center'>
                         <Image src={image1} alt="watch" />
                     </div>
                     <div className={"card__category__description"}>
@@ -27,8 +26,8 @@ export default function CardCategory() {
                         </button>
                     </div>
                 </div>
-                <div className={"card__category__item item-2"}>
-                    <div className={"card__category-img"}>
+                <div className="items-center text-center max-w-[360px] h-[640px] bg-card-category-1">
+                    <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center'>
                         <img src="/images/card_category/image 64.png" alt="watch" />
                     </div>
                     <div className={"card__category__description"}>
@@ -39,8 +38,8 @@ export default function CardCategory() {
                         </button>
                     </div>
                 </div>
-                <div className={"card__category__item item-3"}>
-                    <div className={"card__category-img"}>
+                <div className="items-center text-center max-w-[360px] h-[640px] bg-card-category-2">
+                    <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center'>
                         <img src="/images/card_category/image 41.png" alt="watch" />
                     </div>
                     <div className={"card__category__description"}>
@@ -51,8 +50,8 @@ export default function CardCategory() {
                         </button>
                     </div>
                 </div>
-                <div className={"card__category__item item-4"}>
-                    <div className={"card__category-img"}>
+                <div className="items-center text-center max-w-[360px] h-[640px] bg-card-category-3">
+                    <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center ml-[78px]'>
                         <img src="/images/card_category/Macbook_1.png" alt="watch" />
                     </div>
                     <div className={"card__category__description"}>
@@ -63,59 +62,57 @@ export default function CardCategory() {
                         </button>
                     </div>
                 </div>
-            </div> */}
-            <div>
-                <Carousel showArrows={false} autoPlay={true} interval={3000} infiniteLoop={true} showThumbs={false}>
-                    <div className={"card__category__item item-1"}>
-                        <div className={"card__category-img"}>
-                            <Image src={image1} alt="watch" />
-                        </div>
-                        <div className={"card__category__description"}>
-                            <h3>Popular Products</h3>
-                            <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                            <button className={"btn__black-small"}>
-                                <p>Shop Now</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className={"card__category__item item-2"}>
-                        <div className={"card__category-img"}>
-                            <img src="/images/card_category/image 64.png" alt="watch" />
-                        </div>
-                        <div className={"card__category__description"}>
-                            <h3>Ipad Pro</h3>
-                            <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                            <button className={"btn__black-small"}>
-                                <p>Shop Now</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className={"card__category__item item-3"}>
-                        <div className={"card__category-img"}>
-                            <img src="/images/card_category/image 41.png" alt="watch" />
-                        </div>
-                        <div className={"card__category__description"}>
-                            <h3>Samsung Galaxy </h3>
-                            <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                            <button className={"btn__black-small"}>
-                                <p>Shop Now</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className={"card__category__item item-4"}>
-                        <div className={"card__category-img"}>
-                            <img src="/images/card_category/Macbook_1.png" alt="watch" />
-                        </div>
-                        <div className={"card__category__description"}>
-                            <h3>Macbook Pro</h3>
-                            <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
-                            <button className={"btn__white-small"}>
-                                <p>Shop Now</p>
-                            </button>
-                        </div>
-                    </div>
-                </Carousel>
+            </div >
+        <Carousel showArrows={false} autoPlay={true} interval={3000} infiniteLoop={true} showThumbs={false} className='block md:hidden max-w-[375px] mx-auto'>
+            <div className="items-center text-center max-w-[360px] h-[640px]">
+                <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center'>
+                    <Image src={image1} alt="watch" />
+                </div>
+                <div className={"card__category__description"}>
+                    <h3>Popular Products</h3>
+                    <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <button className={"btn__black-small"}>
+                        <p>Shop Now</p>
+                    </button>
+                </div>
             </div>
-        </section>
+            <div className="items-center text-center max-w-[360px] h-[640px] bg-card-category-1">
+                <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center'>
+                    <img src="/images/card_category/image 64.png" alt="watch" />
+                </div>
+                <div className={"card__category__description"}>
+                    <h3>Ipad Pro</h3>
+                    <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <button className={"btn__black-small"}>
+                        <p>Shop Now</p>
+                    </button>
+                </div>
+            </div>
+            <div className="items-center text-center max-w-[360px] h-[640px] bg-card-category-2">
+                <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center'>
+                    <img src="/images/card_category/image 41.png" alt="watch" />
+                </div>
+                <div className={"card__category__description"}>
+                    <h3>Samsung Galaxy </h3>
+                    <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <button className={"btn__black-small"}>
+                        <p>Shop Now</p>
+                    </button>
+                </div>
+            </div>
+            <div className="items-center text-center max-w-[360px] h-[640px] bg-card-category-3">
+                <div className='bg-contain bg-no-repeat max-w-full max-h-[336px] text-center'>
+                    <img src="/images/card_category/Macbook_1.png" alt="watch" />
+                </div>
+                <div className={"card__category__description"}>
+                    <h3>Macbook Pro</h3>
+                    <p className={"card__category__description-p"}>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <button className={"btn__white-small"}>
+                        <p>Shop Now</p>
+                    </button>
+                </div>
+            </div>
+        </Carousel>
+        </section >
     )
 }
