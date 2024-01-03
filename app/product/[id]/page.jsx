64bfc8bcfa0754e06720a420bc468c220e8c5d89
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-01-04 00:20:29 +0300

    начало верстки детали продукта

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -9,15 +9,52 @@ export default function Product() {
 
             <div className="porduct">
 
-                <div className="breadcrumb-section py-10">
+                <section className="breadcrumb py-10">
                     <div className="container">
-                        <Breadcrumbs/>
+                        <Breadcrumbs />
                     </div>
-                </div>
+                </section>
+
+                <section className="product__info py-28">
+                    <div className="container">
+                        <div className="flex flex-row h-[672px]">
+
+                                <div className="flex gap-x-12 my-auto">
+                                    <ul className="flex flex-col gap-y-6 overflow-auto h-full w-20">
+                                        <li>
+                                            <img src="../images/products/Iphone14pro-1.png" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="../images/products/Iphone14pro-1.png" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="../images/products/Iphone14pro-1.png" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="../images/products/Iphone14pro-1.png" alt="" />
+                                        </li>
+                                        <li>
+                                            <img src="../images/products/Iphone14pro-1.png" alt="" />
+                                        </li>
+                                    </ul>
+                                    
+                                    <div className="max-w-[413px]">
+                                        <img className="w-full" src="../images/products/Iphone14pro-1.png" alt="product-photo" />
+                                    </div>
+                                </div>
+
+                            <div className="">
+
+                            </div>
+                        </div>
+
+                        <h1>Product</h1>
+
+                    </div>
+                </section>
 
             </div>
 
-            <h1>Product</h1>
 
         </MainLayout>
     )
