401b0a53d466f1b1d5e4fee4a200eef0fd65434d
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-01-06 21:54:36 +0300

    обертка ссылки по id

--- app/ui/components/products/product/Product.jsx
+++ app/ui/components/products/product/Product.jsx
@@ -10,16 +10,15 @@ export default function Product({ product }) {
     return (
         <div className="product__cart product">
             <Image className="product__cart-icon" src={favoriteIcon} alt="favourite-icon" />
-            <a 
-            className='flex flex-col items-center' 
-            href={`/product/${product.id}`}>
-                <div className="product__image">
-                    <img src={product.image} alt="product-image" />
+            <a
+                href={`/product/${product.id}`}>
+                <div className='flex flex-col gap-y-4 items-center'>
+                    <div className="product__image">
+                        <img src={product.image} alt="product-image" />
+                    </div>
+                    <div className="product__cart-title grow"><p>{product.name}</p></div>
+                    <div className="product__cart-price">$ {product.price}</div>
                 </div>
-
-                <div className="product__cart-title grow"><p>{product.name}</p></div>
-                <div className="product__cart-price">$ {product.price}</div>
-
             </a>
             <button className="button btn__black mx-auto">
                 <p>В корзину</p>
