5919b15c1a419aa54dc78fdff4b569292d9dd32a
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-01-06 20:04:11 +0300

    ссылка на страеницу продукта

--- app/ui/components/products/product/Product.jsx
+++ app/ui/components/products/product/Product.jsx
@@ -6,16 +6,21 @@ import favoriteIcon from '/public/images/Icons/Favorite_duotone-red.svg'
 
 
 
-export default function Product( {product} ) {
+export default function Product({ product }) {
     return (
         <div className="product__cart product">
-            <Image className="product__cart-icon" src={favoriteIcon} alt="favourite-icon"/>
-            <div className="product__image">
-                <img  src={product.image} alt="product-image"/>
-            </div>
-            
-            <div className="product__cart-title grow"><p>{product.name}</p></div>
-            <div className="product__cart-price">$ {product.price}</div>
+            <Image className="product__cart-icon" src={favoriteIcon} alt="favourite-icon" />
+            <a 
+            className='flex flex-col items-center' 
+            href={`/product/${product.id}`}>
+                <div className="product__image">
+                    <img src={product.image} alt="product-image" />
+                </div>
+
+                <div className="product__cart-title grow"><p>{product.name}</p></div>
+                <div className="product__cart-price">$ {product.price}</div>
+
+            </a>
             <button className="button btn__black mx-auto">
                 <p>В корзину</p>
             </button>
