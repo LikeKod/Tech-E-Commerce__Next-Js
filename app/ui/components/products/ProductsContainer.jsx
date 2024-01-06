6c086293f4dc2bfb6665f0c3dd0f62ad729303c7
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-01-06 21:54:15 +0300

    justify center для карточек в каталоге

--- app/ui/components/products/ProductsContainer.jsx
+++ app/ui/components/products/ProductsContainer.jsx
@@ -5,7 +5,7 @@ import Product from "./product/Product"
 
 export default function ProductsContainer({ products }) {
     return (
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap justify-center gap-4">
             {products.map((product) =>
                 <Product key={product.id} product={product} />
             )}
