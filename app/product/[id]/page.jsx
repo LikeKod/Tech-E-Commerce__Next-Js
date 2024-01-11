93069ea722a8a4732a8657f44fb52b636ca29efa
Olesya-OM <tmehrakovao5@gmail.com>
2024-01-11 21:25:03 +0300

    sum pay

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,11 +1,12 @@
 'use client'
 
 import MainLayout from "../../layouts/MainLayout"
+import Shipping from "../../orders/sum__pay/Shipping"
 
 export default function Product() {
     return (
         <MainLayout>
-
+        <Shipping />
         </MainLayout>
     )
 }
