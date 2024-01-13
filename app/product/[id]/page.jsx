f1bb75867ee191f589adfc5e0993d159809a79e3
Olesya-OM <tmehrakovao5@gmail.com>
2024-01-13 15:23:26 +0300

    .

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import MainLayout from "../../layouts/MainLayout"
-import Shipping from "../../orders/sum__pay/Shipping"
+
 import Payment from "../../orders/sum__pay/Payment"
 
 export default function Product() {
