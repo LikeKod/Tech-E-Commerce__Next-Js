cebb4e13f764e49a49724a3eea7495aad573fbc4
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-02-08 17:05:30 +0300

    стили слайдера категорий

--- app/ui/Slider/Slider.jsx
+++ app/ui/Slider/Slider.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useRef,useState} from 'react'
+import React, { useRef, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import Image from 'next/image'
@@ -18,71 +18,86 @@ import Headphones from '/public/images/Icons/Headphones-48.svg'
 import Computers from '/public/images/Icons/Computers-48.svg'
 import Gaming from '/public/images/Icons/Gaming-48.svg'
 
-
 export default function Slider() {
-  return (
-    <div className="Slider">
-      <Swiper
-      slidesPerView={2}
-      spaceBetween={16}
-      navigation={{
-        nextEl: '.review-swiper-button-next',
-        prevEl: '.review-swiper-button-prev'
-      }}
-      breakpoints={{
-        400: {
-          slidesPerView: 3,
-          spaceBetween: 16,
-        },
-        600: {
-          slidesPerView: 4,
-          spaceBetween: 16,
-        },
-        900: {
-          slidesPerView: 5,
-          spaceBetween: 32,
-        },
-        1100: {
-          slidesPerView: 6,
-          spaceBetween: 32,
-        }
-        
-      }}
-      modules={[Grid, Navigation]}
-      className='mySwiper'
-        >
-        <BtnSlider className="change slider" />
-        <SwiperSlide>
-        <Image className='Slider-img' src={Phones} alt="Phones" style={{width: '48'}} />
-        <div className="text">Phones</div>
-        </SwiperSlide>
-      <SwiperSlide>
-        <Image className='Slider-img' src={SmartWatches} alt="Smart Watches" style={{width: '48'}} />
-        <div className="text">Smart Watches</div>
-        </SwiperSlide>
-      <SwiperSlide>
-        <Image className='Slider-img' src={Cameras} alt="Cameras" style={{width: '48'}} />
-        <div className="text">Cameras</div>
-        </SwiperSlide>
-      <SwiperSlide>
-        <Image className='Slider-img' src={Headphones} alt="Headphones" style={{width: '48'}} />
-        <div className="text">Headphones</div>
-        </SwiperSlide>
-      <SwiperSlide>
-        <Image className='Slider-img' src={Computers} alt="Computers" style={{width: '48'}} />
-        <div className="text">Computers</div>
-        </SwiperSlide>
-      <SwiperSlide>
-        <Image className='Slider-img' src={Gaming} alt="Gaming" style={{width: '48'}} />
-        <div className="text">Gaming</div>
-        </SwiperSlide>
-      <SwiperSlide>
-        <Image className='Slider-img' src={Gaming} alt="Gaming" style={{width: '48'}} />
-        <div className="text">Gaming</div>
-        </SwiperSlide>
-    </Swiper>
-    </div>
-  );
+    return (
+        <div className="slider container my-20">
+            <Swiper
+                slidesPerView={6}
+                loop={true}
+                breakpoints={{
+                    '@0.00': {
+                        slidesPerView: 3,
+                        spaceBetween: 16,
+                    },
+                    '@0.50': {
+                        slidesPerView: 4,
+                        spaceBetween: 16,
+                    },
+                    '@0.75': {
+                        slidesPerView: 5,
+                        spaceBetween: 32,
+                    },
+                    '@1.0': {
+                        slidesPerView: 6,
+                        spaceBetween: 32,
+                    }
+
+                }}
+                modules={[Navigation]}
+                className='mySwiper'
+            >
+                <BtnSlider className="change slider" />
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={Phones} alt="Phones" />
+                        <div className="text">Phones</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={SmartWatches} alt="Smart Watches" style={{ width: '48' }} />
+                        <div className="text">Smart Watches</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={Cameras} alt="Cameras" style={{ width: '48' }} />
+                        <div className="text">Cameras</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={Headphones} alt="Headphones" style={{ width: '48' }} />
+                        <div className="text">Headphones</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={Computers} alt="Computers" style={{ width: '48' }} />
+                        <div className="text">Computers</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={Gaming} alt="Gaming" style={{ width: '48' }} />
+                        <div className="text">Gaming</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={Gaming} alt="Gaming" style={{ width: '48' }} />
+                        <div className="text">Gaming</div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide-background">
+                        <Image className='Slider-img' src={Gaming} alt="Gaming" style={{ width: '48' }} />
+                        <div className="text">Gaming</div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
 }
 
 
