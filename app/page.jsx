a84b6bea78a728c323649ead869c3813d2d75183
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-02-09 18:20:05 +0300

    раскоментировал закоментированные компоненты

--- app/page.jsx
+++ app/page.jsx
@@ -21,7 +21,7 @@ export default async function Home() {
 
     return (
         <MainLayout >
-            {/* <Banner /> */}
+            <Banner />
             <Banners />
             <Slider />
 
