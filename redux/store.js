0b1000bb62daa08609eb428ecfee09565eb01ec6
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-02-15 11:48:01 +0300

    added persist lib for storage data

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartReduser from './slices/cartSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './slices/cartSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+import { combineReducers } from 'redux';
 
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReduser,
-  },
-})
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
